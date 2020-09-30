682179067920f38175ebad0c0d585b57b5a04eb4
mstoops <mstoops@student.21-school.ru>
2020-09-30 22:36:17 +0300

    Layout and dependencies were made

--- app/src/main/java/com/akhmilyas/rxfibonacci/MainActivity.kt
+++ app/src/main/java/com/akhmilyas/rxfibonacci/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.akhmilyas.rxfibonacci
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        for (i in 0..30) {
+            val txt1 = TextView(this)
+            txt1.text = "$i"
+            fib_output.addView(txt1)
+        }
     }
 }
