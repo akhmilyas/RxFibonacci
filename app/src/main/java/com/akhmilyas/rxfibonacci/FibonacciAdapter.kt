5639f2f3cfa082881817ddc20937c2a6127e9970
mstoops <mstoops@student.21-school.ru>
2020-10-09 17:03:08 +0300

    Заменил ArrayList на List, исправил кодстайл

--- app/src/main/java/com/akhmilyas/rxfibonacci/FibonacciAdapter.kt
+++ app/src/main/java/com/akhmilyas/rxfibonacci/FibonacciAdapter.kt
@@ -5,13 +5,15 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class FibonacciAdapter(private val fibNumbers: ArrayList<String>) :
+class FibonacciAdapter(private val fibNumbers: List<String>) :
     RecyclerView.Adapter<FibonacciAdapter.FibonacciViewHolder>() {
 
     class FibonacciViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 
-    override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): FibonacciViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): FibonacciViewHolder {
         val textView = LayoutInflater.from(parent.context)
             .inflate(R.layout.fib_number, parent, false) as TextView
 
